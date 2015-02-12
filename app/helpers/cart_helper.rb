fd97845a67d3e5f1da2abaac78067362c0a58693
Rozsenich Balazs <balazs@rozsenich.hu>
2015-02-12 11:50:31 +0100

    guest-booking association

---
+++ app/helpers/cart_helper.rb
@@ -0,0 +1,23 @@
+module CartHelper
+  def create_cart_for(guest)
+    return create_cart_for(guest)
+  end
+
+  def self.create_cart_for(guest)
+    cart = Booking.new
+    cart.state = 'CART'
+    guest.bookings.push(cart)
+  end
+
+  def self.has_cart?(guest)
+    guest.bookings.each do |b|
+      return true if b.state == 'CART'
+    end
+
+    false
+  end
+
+  def has_cart?(guest)
+    return self.has_cart?(guest)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211210146) do
+ActiveRecord::Schema.define(version: 20150212104021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,20 @@ ActiveRecord::Schema.define(version: 20150211210146) do
     t.integer  "guest_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "bookings_guests", force: :cascade do |t|
+    t.integer  "guest_id"
+    t.integer  "booking_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bookings_rooms", force: :cascade do |t|
+    t.integer  "booking_id"
+    t.integer  "room_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "categries", force: :cascade do |t|
