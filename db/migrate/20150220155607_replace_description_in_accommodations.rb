6587f3cee110e9d913e8691fa7c77704c3c402dd
Rozsenich Balázs <m3dw3@users.noreply.github.com>
2015-02-20 18:08:24 +0100

    Delete 20150220155607_replace_description_in_accommodations.rb

--- db/migrate/20150220155607_replace_description_in_accommodations.rb
+++
@@ -1,6 +0,0 @@
-class ReplaceDescriptionInAccommodations < ActiveRecord::Migration
-  def change
-    remove_column :accommodations, :description, :text
-    add_column :accommodations, :description, :text
-  end
-end
