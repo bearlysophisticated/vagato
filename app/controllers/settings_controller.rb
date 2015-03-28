c50174de57907479082f79ecccfc9a5b7af045e4
Rozsenich Balázs <b.rozsenich@gmail.com>
2015-03-28 13:59:29 +0100

    saving app settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -11,6 +11,19 @@ class SettingsController < ApplicationController
   end
 
   def update
+    property = Property.find_by_key(params[:property][:key])
+    unless property.nil?
+      property.value = params[:property][:value]
+      if property.save!
+        flash[:notice] = 'A beállítás mentve!'
+      else
+        flash[:warning] = 'Sikertelen mentés'
+      end
+    else
+      flash[:warning] = 'Sikertelen mentés'
+    end
 
+    redirect_to '/settings'
   end
+
 end
