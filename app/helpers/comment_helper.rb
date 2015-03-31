a52d31797c0cf22ffee58b3eea6e1adef991a64c
Rozsenich Balázs <b.rozsenich@gmail.com>
2015-03-31 15:06:45 +0200

    average rating is included in optimization model

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -1,10 +1,10 @@
 module CommentHelper
 
-  def get_average_stars_for(criteria)
+  def self.get_average_stars_for(criteria)
     if criteria.is_a? Accommodation
       return self.get_average_stars_for_accommodation(criteria)
     elsif criteria.is_a? Room
-      return get_average_stars_for_room(criteria)
+      return self.get_average_stars_for_room(criteria)
     end
   end
 
@@ -15,7 +15,7 @@ module CommentHelper
       avg_stars += c.stars
     end
 
-    return avg_stars/accommodation.comments.size
+    return accommodation.comments.size > 0 ? avg_stars/accommodation.comments.size : 0
   end
 
 
@@ -25,7 +25,7 @@ module CommentHelper
       avg_stars += c.stars
     end
 
-    return avg_stars/room.accommodation.comments.size
+    return room.accommodation.comments.size > 0 ?avg_stars/room.accommodation.comments.size : 0
   end
 
 end
