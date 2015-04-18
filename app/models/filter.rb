16e93fe79ad0f529a4a7378fa3d2045a704b74d9
Rozsenich Balázs <b.rozsenich@gmail.com>
2015-04-18 23:34:41 +0200

    cheap default value

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -25,7 +25,7 @@ class Filter < ActiveRecord::Base
     self.two_bed = params[:two_bed] if params.has_key? :two_bed
     self.three_bed = params[:three_bed] if params.has_key? :three_bed
     self.four_or_more_bed = params[:four_or_more_bed] if params.has_key? :four_or_more_bed
-    self.cheap = params[:cheap] if params.has_key? :cheap
+    self.cheap = params.has_key?(:cheap) || (!params.has_key?(:cheap) && !params.has_key?(:close))
     self.close = params[:close] if params.has_key? :close
   end
 end
